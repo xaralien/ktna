6111738aea4fbf21059fb101576aae40d6c69919
xaralien <dimasuzumaki126@gmail.com>
2024-11-29 09:27:08 +0700

    update

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'ktna.co.id',
+	'username' => 'ktnacoid_user',
+	'password' => 'ktna123!@#',
+	'database' => 'ktnacoid_core',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
