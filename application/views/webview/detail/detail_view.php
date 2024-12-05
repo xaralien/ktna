07bab38d177c1784abddd8d7ac36d560eca818a8
xaralien <dimasuzumaki126@gmail.com>
2024-12-05 09:19:25 +0700

    Update href

--- application/views/webview/detail/detail_view.php
+++ application/views/webview/detail/detail_view.php
@@ -76,43 +76,9 @@
                             <img class="img-fluid" src="<?= base_url('assets/') ?>img/blog/single_blog_1.png" alt="">
                         </div>
                         <div class="blog_details">
-                            <h2>Second divided from form fish beast made every of seas
-                                all gathered us saying he our
+                            <h2>
+                                Artikel Not Found 404
                             </h2>
-                            <ul class="blog-info-link mt-3 mb-4">
-                                <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle</a></li>
-                                <li><a href="#"><i class="fa fa-comments"></i> 03 Comments</a></li>
-                            </ul>
-                            <p class="excert">
-                                MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                                should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                                fraction of the camp price. However, who has the willpower
-                            </p>
-                            <p>
-                                MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                                should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                                fraction of the camp price. However, who has the willpower to actually sit through a
-                                self-imposed MCSE training. who has the willpower to actually
-                            </p>
-                            <div class="quote-wrapper">
-                                <div class="quotes">
-                                    MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                                    should have to spend money on boot camp when you can get the MCSE study materials yourself at
-                                    a fraction of the camp price. However, who has the willpower to actually sit through a
-                                    self-imposed MCSE training.
-                                </div>
-                            </div>
-                            <p>
-                                MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                                should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                                fraction of the camp price. However, who has the willpower
-                            </p>
-                            <p>
-                                MCSE boot camps have its supporters and its detractors. Some people do not understand why you
-                                should have to spend money on boot camp when you can get the MCSE study materials yourself at a
-                                fraction of the camp price. However, who has the willpower to actually sit through a
-                                self-imposed MCSE training. who has the willpower to actually
-                            </p>
                         </div>
                     </div>
                 <?php
@@ -227,7 +193,7 @@
                                 <div class="media post_item">
                                     <img src="<?= base_url('uploads/artikel/') . '' . $r->thumbnail ?>" alt="post">
                                     <div class="media-body">
-                                        <a href="single-blog.html">
+                                        <a href="<?= base_url('detail/artikel/' . $c->Id) ?>">
                                             <h3><?= $r->title ?></h3>
                                         </a>
                                         <?php
