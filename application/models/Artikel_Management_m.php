647668f5d87eef96806ae2eb495696b072ff764a
xaralien <dimasuzumaki126@gmail.com>
2024-12-05 09:37:46 +0700

    update artikel management

--- application/models/Artikel_Management_m.php
+++ application/models/Artikel_Management_m.php
@@ -3,8 +3,8 @@
 class Artikel_Management_m extends CI_Model
 {
     var $table = 'artikel';
-    var $column_order = array('Id', 'category', 'title', 'thumbnail', 'tanggal', 'count'); //set column field database for datatable orderable
-    var $column_search = array('Id', 'category', 'title', 'thumbnail', 'tanggal', 'count'); //set column field database for datatable searchable 
+    var $column_order = array('Id', 'category', 'title', 'thumbnail', 'tanggal', 'view_count'); //set column field database for datatable orderable
+    var $column_search = array('Id', 'category', 'title', 'thumbnail', 'tanggal', 'view_count'); //set column field database for datatable searchable 
     var $order = array('artikel.tanggal' => 'DESC'); // default order 
 
     function _get_datatables_query()
